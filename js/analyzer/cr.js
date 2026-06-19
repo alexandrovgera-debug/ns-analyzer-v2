f5036d1e521b1af0bc6116d2424f980942df43f1
alexandrovgera-debug <alexandrovgera@gmail.com>
2026-06-20 02:16:41 +0300

    Create cr.js

---
+++ js/analyzer/cr.js
@@ -0,0 +1,79 @@
+export default class CRAnalyzer {
+
+    constructor(profile) {
+        this.profile = profile;
+    }
+
+    // === средний SR по интервалам CR ===
+    buildIntervalSR(srRows) {
+
+        const cr = this.profile?.cr || [];
+
+        const result = [];
+
+        for (let i = 0; i < cr.length; i++) {
+
+            const current = cr[i];
+            const next = cr[i + 1];
+
+            const start = this.toHour(current.time);
+            const end = next ? this.toHour(next.time) : 24;
+
+            let sum = 0;
+            let count = 0;
+
+            for (const row of srRows || []) {
+
+                const time = new Date(row.time);
+                const hour = time.getHours() + time.getMinutes() / 60;
+
+                if (hour >= start && hour < end) {
+                    sum += (row.sr ?? 1);
+                    count += 1;
+                }
+            }
+
+            const avg = count ? sum / count : 1;
+
+            result.push({
+                start,
+                end,
+                srAvg: avg,
+                cr: current.value
+            });
+        }
+
+        return result;
+    }
+
+    // === расчет CR ===
+    calculateCR(intervalSR) {
+
+        const result = [];
+
+        for (const i of intervalSR) {
+
+            const currentCR = i.cr;
+
+            // SR > 1 → чувствительность выше → углеводы "сильнее" → CR уменьшается
+            const factor = 1 / (i.srAvg || 1);
+
+            result.push({
+                start: i.start,
+                end: i.end,
+                currentCR,
+                suggestedCR: currentCR * factor
+            });
+
+        }
+
+        return result;
+    }
+
+    toHour(timeStr) {
+
+        const [h, m] = timeStr.split(":").map(Number);
+
+        return h + m / 60;
+    }
+}
